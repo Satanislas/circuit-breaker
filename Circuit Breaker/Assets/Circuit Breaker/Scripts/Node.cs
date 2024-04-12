6f12b10ced4e49d84b87ab791f5646a258bc311b
afonc <andaguilar@csumb.edu>
2024-04-11 21:02:01 -0700

    Wire and Node Functionality
    Node
    - Added a Wire[] to store connected wires
    - Added GetNextNode()
    
    Wire
    - Setup variables with helpful tooltips on how to use them
    - Draw between nodes successfully
    - Added GetOtherNode()

---
+++ Circuit Breaker/Assets/Circuit Breaker/Scripts/Node.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [Header("Required")]
+    [Tooltip("Fill with all the wires that connect to this node. Should never be empty.")]
+    public Wire[] connectedWires;
+
+    public int ConnectionNum
+    {
+        get { return connectedWires.Length; }
+    }
+
+    // returns the next node along a given wire, indicated by wireIndex
+    public Transform GetNextNode(int wireIndex)
+    {
+        // sanity checks
+        if(wireIndex >= connectedWires.Length || wireIndex < 0)
+        {
+            Debug.Log(wireIndex + "Not within connectedWires index range.");
+            return null;
+        }
+
+        Transform otherNode = connectedWires[wireIndex].GetOtherNode(this.transform);
+        Debug.Log(name + " is connected to " + otherNode.name + " by " + connectedWires[wireIndex].name);
+        return otherNode;
+    }
+}
--- Circuit Breaker/Assets/Circuit Breaker/Scripts/Wire.cs
+++ Circuit Breaker/Assets/Circuit Breaker/Scripts/Wire.cs
@@ -16,7 +16,14 @@ public class Wire : MonoBehaviour
     public Transform tileSpot;
 
     [Header("Optional")]
-    // public Component component
+    [Tooltip("Component to default to on wire. Fill with a CircuitComponent prefab.")]
+    public GameObject componentToSpawnWith;
+    [Tooltip("Turn on to disable changing a component once it's placed")]
+    public bool isLocked;
+
+    [HideInInspector]
+    public bool isOpen;
+    public Transform activeComponent;
 
     private LineRenderer lineRenderer;
     
@@ -24,11 +31,30 @@ public class Wire : MonoBehaviour
     {
         lineRenderer = GetComponent<LineRenderer>();
 
-
         DrawLine();
         PositionTileSpot();
+        SpawnComponent();
     }
 
+    // spawns a default component if needed
+    public void SpawnComponent()
+    {
+        // check if we should spawn with a component
+        if (componentToSpawnWith == null)
+        {
+            return;
+        }
+
+        // check if component already on wire
+        if (activeComponent != null)
+        {
+            return;
+        }
+
+        Instantiate(componentToSpawnWith, tileSpot.position, tileSpot.rotation);
+    }
+
+    // draws the line between nodes
     public void DrawLine()
     {
         lineRenderer.SetPosition(0, nodeOne.position);
@@ -36,10 +62,28 @@ public class Wire : MonoBehaviour
         lineRenderer.enabled = true;
     }
 
+    // positions the tile graphic
     public void PositionTileSpot()
     {
         Vector3 pos = (nodeOne.position + nodeTwo.position) / 2f;
         tileSpot.position = pos;
         tileSpot.LookAt(nodeOne);
+    }
+
+    // if the given node is connected to this wire, return the other node.
+    // if not, return null
+    public Transform GetOtherNode(Transform node)
+    {
+        if(node == nodeOne)
+        {
+            return nodeTwo;
+        }
+
+        if (node == nodeTwo)
+        {
+            return nodeOne;
+        }
+
+        return null;
     }
 }
