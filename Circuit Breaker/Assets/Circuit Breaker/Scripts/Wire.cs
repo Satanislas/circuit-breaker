ad9e64ab71a5b57a7f6c42d6b9ca1d4057f63976
afonc <andaguilar@csumb.edu>
2024-04-12 16:14:10 -0700

    Update Wire.cs
    Reworked how wires store their nodes to support future functionality for a polarized wire.

--- Circuit Breaker/Assets/Circuit Breaker/Scripts/Wire.cs
+++ Circuit Breaker/Assets/Circuit Breaker/Scripts/Wire.cs
@@ -6,26 +6,26 @@ using UnityEngine;
 public class Wire : MonoBehaviour
 {
     [Header("Required")]
-    [Tooltip("The first node to connect to.\nDo not leave empty.")]
-    public Transform nodeOne;
-    [Tooltip("The second node to connect to.\nDo not leave empty.")]
-    public Transform nodeTwo;
+    [Tooltip("Nodes this wire connects. Will only ever be two nodes.\nDo not leave empty.")]
+    public Transform[] nodes;
 
     [Header("Prefab Settings")]
     [Tooltip("The transform that controls where the tile is located.\nCan also be used to position components.")]
     public Transform tileSpot;
     [Range(0,1)]
-    [Tooltip("Specifies where on the wire the tileSpot resides.\nCloser to 0 indicates closer to nodeOne.\nCloser to 1 indicates closer to nodeTwo.\n0.5 indicates perfectly in the middle.")]
+    [Tooltip("Specifies where on the wire the tileSpot resides.\nCloser to 0 indicates closer to nodes[0].\nCloser to 1 indicates closer to nodes[1].\n0.5 indicates perfectly in the middle.")]
     public float tileOffset;
 
     [Header("Optional")]
     [Tooltip("Component to default to on wire.\nFill with a CircuitComponent prefab.")]
     public GameObject defaultComponent;
-    [Tooltip("Turn on to disable changing a component once it's placed")]
+    [Tooltip("Turn on to disable changing a component once it's placed.")]
     public bool isLocked;
-
-    [HideInInspector]
+    [Tooltip("Turn on to force a Spark to only traverse from nodes[0] to nodes[1].\nEffectively disables reverse traversal.")]
+    public bool isPolarized;
+    [Tooltip("Turn on to open this wire. Will not allow traversal across in any direction.")]
     public bool isOpen;
+    
     [HideInInspector]
     private Transform activeComponent;
 
@@ -66,20 +66,20 @@ public class Wire : MonoBehaviour
     // draws the line between nodes
     public void DrawLine()
     {
-        lineRenderer.SetPosition(0, nodeOne.position);
-        lineRenderer.SetPosition(1, nodeTwo.position);
+        lineRenderer.SetPosition(0, nodes[0].position);
+        lineRenderer.SetPosition(1, nodes[1].position);
         lineRenderer.enabled = true;
     }
 
     // positions the tile graphic
     public void PositionTileSpot()
     {
-        float lerpX = Mathf.Lerp(nodeOne.position.x, nodeTwo.position.x, tileOffset);
-        float lerpY = Mathf.Lerp(nodeOne.position.y, nodeTwo.position.y, tileOffset);
+        float lerpX = Mathf.Lerp(nodes[0].position.x, nodes[1].position.x, tileOffset);
+        float lerpY = Mathf.Lerp(nodes[0].position.y, nodes[1].position.y, tileOffset);
 
         Vector3 pos = new Vector3(lerpX, lerpY, 0f);
         tileSpot.position = pos;
-        tileSpot.LookAt(nodeOne);
+        tileSpot.LookAt(nodes[0]);
     }
 
     // if the given node is connected to this wire, return the other node.
@@ -91,18 +91,84 @@ public class Wire : MonoBehaviour
             return null;
         }
 
-        if(node == nodeOne)
+        if(node == nodes[0])
         {
-            return nodeTwo;
+            return nodes[1];
         }
         
-        return nodeOne;
+        return nodes[0];
     }
 
     // returns true if the given node is one of the nodes this wire connects
     public bool IsConnectedTo(Transform node)
     {
-        return node == nodeOne || node == nodeTwo;
+        return node == nodes[0] || node == nodes[1];
+    }
+
+    public void SetPolarized(int i)
+    {
+        // sanity check
+        if(i != 0 || i != 1)
+        {
+            Debug.Log(i + " is not one of the two nodes.");
+            return;
+        }
+
+        isPolarized = true;
+
+        // if the provided index is 1, swap the node order in nodes[].
+        if(i == 1)
+        {
+            Transform temp = nodes[0];
+            nodes[0] = nodes[1];
+            nodes[1] = temp;
+        }
+    }
+
+    public void DisablePolarization()
+    {
+        isPolarized = false;
+    }
+
+    // Given a node, will return the index of the node within nodes[]
+    // if not found, and therefore is not conected, returns -1
+    public int GetNodeIndex(Transform node)
+    {
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            if (nodes[i] == node)
+            {
+                Debug.Log(node.name + " has an index of " + i + " in " + name + "'s nodes.");
+                return i;
+            }
+        }
+
+        Debug.Log(node.name + " is not connected to " + name);
+        return -1;
+    }
+
+    // Given a node, will return whether or not it is possible to traverse this wire starting from the given node.
+    public bool CanTraverse(Transform node)
+    {
+        if (isOpen)
+        {
+            return false;
+        }
+
+        if (!IsConnectedTo(node))
+        {
+            return false;
+        }
+
+        if (isPolarized)
+        {
+            if(GetNodeIndex(node) != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /*
