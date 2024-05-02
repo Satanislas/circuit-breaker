c3946b7e23ec23e30d8453956cfb9e4fce31a0ff
Sebastian Echeverria <secheverriamartinez@csumb.edu>
2024-05-01 20:00:18 -0700

    Fixed Conflicts

--- Circuit Breaker/Assets/Circuit Breaker/Main Menu Scripts/MainMenu.cs
+++ Circuit Breaker/Assets/Circuit Breaker/Main Menu Scripts/MainMenu.cs
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-
-public class MainMenu : MonoBehaviour
-{
-    public Button[] buttons;
-    void Awake()
-    {
-        if(!PlayerPrefs.HasKey("Level1"))
-        {
-            PlayerPrefs.SetInt("Level1", 0);
-        }
-    }
-    public void Play()
-    {
-        Debug.Log("GOING TO LEVEL SELECTOR");
-        
-        // SceneManager.LoadScene("LevelSelector");
-    }
-    public void Quit()
-    {
-        Debug.Log("QUITTING GAME");
-        // Application.Quit();
-    }
-
-    
-
-    
-}
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -50,4 +16,3 @@ public class MainMenu : MonoBehaviour
         // Application.Quit();
     }
 }
->>>>>>> a4bf6678dba6ad2c96f5d778cbc9f4b6b9d7a36e
